404074134ae966bfe9b8283c1842f40ce4de59c4
Aleksey Makarkin <aleksey.makarkin@gmail.com>
2021-03-06 12:34:44 +0300

    Eagle1

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0xf3fFA652f64e7b4B7327580909c143af23817fB6',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0xbd33A691732f6D7c0b70B048b4E16A9F68C03ca1'
 }
 
 export default priceContracts
